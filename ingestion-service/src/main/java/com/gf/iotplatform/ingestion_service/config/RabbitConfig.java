2becb3e6280af3ad63dcb68e2eaa26e04b5fb312
Gabriel Leal <gabrielleaal13@gmail.com>
2026-05-01 18:34:25 -0300

    Criação docker compose v1

--- ingestion-service/src/main/java/com/gf/iotplatform/ingestion_service/config/RabbitConfig.java
+++ ingestion-service/src/main/java/com/gf/iotplatform/ingestion_service/config/RabbitConfig.java
@@ -1,29 +1,67 @@
 package com.gf.iotplatform.ingestion_service.config;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-
 @Configuration
 public class RabbitConfig {
 
+    // =========================
+    // FILA / EXCHANGE / ROUTING
+    // =========================
+
+    public static final String QUEUE = "historical-temp-queue";
+    public static final String EXCHANGE = "temperature.received.event";
+    public static final String ROUTING_KEY = "temperature-received";
+
+    // =========================
+    // CONVERSOR JSON
+    // =========================
+
     @Bean
     public MessageConverter messageConverter() {
-        // usa ObjectMapper interno do Spring automaticamente
         return new Jackson2JsonMessageConverter();
     }
 
+    // =========================
+    // RABBIT TEMPLATE
+    // =========================
+
     @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory,
-                                      MessageConverter messageConverter) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
 
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
         return template;
+    }
+
+    // =========================
+    // DECLARAÇÃO AUTOMÁTICA RABBITMQ
+    // =========================
+
+    @Bean
+    public Queue historicalTempQueue() {
+        return QueueBuilder.durable(QUEUE).build();
+    }
+
+    @Bean
+    public DirectExchange temperatureExchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding bindingHistoricalTempQueue(Queue historicalTempQueue,
+                                              DirectExchange temperatureExchange) {
+
+        return BindingBuilder
+                .bind(historicalTempQueue)
+                .to(temperatureExchange)
+                .with(ROUTING_KEY);
     }
 }
